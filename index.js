0f87418d8b14ad1680605ef684f772622f1cbf7d
niklasfrick <niklas@Niklas-MBP.fritz.box>
2023-09-13 11:47:47 +0200

    :recycle: chore: Checking function refactored to switch statements

--- index.js
+++ index.js
@@ -5,10 +5,8 @@ const TelegramBot = require("node-telegram-bot-api");
 
 // Set Telegram bot token from .env
 const BOT_TOKEN = process.env.BOT_TOKEN;
-
 // Initialize the Telegram bot
 const telegramBot = new TelegramBot(BOT_TOKEN, { polling: false }); // Set polling to false for production use
-
 // URL to monitor
 const MONITOR_URL = process.env.MONITOR_URL;
 
@@ -33,7 +31,10 @@ function sendTelegramNotification(message) {
   // set chat ID variable from .env
   const chatId = process.env.CHAT_ID;
 
-  telegramBot.sendMessage(chatId, message);
+  // create the message with the URL
+  const textMessage = `${message} ${MONITOR_URL}`;
+
+  telegramBot.sendMessage(chatId, textMessage);
 }
 
 async function checkWebsiteForChanges() {
@@ -41,18 +42,22 @@ async function checkWebsiteForChanges() {
     // Fetch website content
     const response = await axios.get(MONITOR_URL);
 
-    if (response.status === 200) {
-      const currentContent = response.data;
+    switch (response.status) {
+      case 200:
+        const currentContent = response.data;
 
-      if (currentContent !== initialContent) {
-        // If changes detected, send a Telegram notification
-        sendTelegramNotification("Website content has changed!");
-        initialContent = currentContent;
-      } else {
-        console.log("No changes detected.");
-      }
-    } else {
-      console.error(`Error fetching website. Status code: ${response.status}`);
+        if (currentContent !== initialContent) {
+          // If changes detected, send a Telegram notification
+          sendTelegramNotification("Website content has changed at:");
+          initialContent = currentContent;
+        }
+        break;
+
+      default:
+        console.error(
+          `Error fetching website. Status code: ${response.status}`
+        );
+        break;
     }
   } catch (error) {
     console.error("An error occurred:", error);
@@ -62,5 +67,5 @@ async function checkWebsiteForChanges() {
 // Run the fetchInitialContent function to populate initialContent, then start checking for changes
 fetchInitialContent().then(() => {
   // Run the checkWebsiteForChanges function periodically (e.g., every 15 minutes)
-  setInterval(checkWebsiteForChanges, 10000); // 15 minutes in milliseconds
+  setInterval(checkWebsiteForChanges, 900000); // 15 minutes in milliseconds
 });
